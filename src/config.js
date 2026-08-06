4818af8b82e7149dd0113c537fc66fefc5239cee
xucheng2024 <eatfreshapple@gmail.com>
2026-08-06 10:52:09 +0800

    Fix Cloudflare trigger reconciliation dispatch

--- src/config.js
+++ src/config.js
@@ -2,6 +2,9 @@ export const CRON_TASKS = new Map([
   ["1,6,11,16,21,26,31,36,41,46,51,56 * * * *", ["monitor_delist", "cancel_pending_limits", "fetch_filled_orders"]],
   ["0,15,30,45 * * * *", ["auto_sell_orders"]],
   ["10 16 * * *", ["fetch_filled_orders"]],
+  // 23:55 SGT = 15:55 UTC, 00:05 SGT = 16:05 UTC
+  ["55 15 * * *", ["dispatch_reconcile_triggers_cancel"]],
+  ["5 16 * * *", ["dispatch_reconcile_triggers_rebuild"]],
 ]);
 
 export const ALLOWED_TASKS = new Set([
@@ -9,6 +12,8 @@ export const ALLOWED_TASKS = new Set([
   "cancel_pending_limits",
   "fetch_filled_orders",
   "auto_sell_orders",
+  "dispatch_reconcile_triggers_cancel",
+  "dispatch_reconcile_triggers_rebuild",
 ]);
 
 export const MAX_ACTIVE_POSITIONS = 3;
